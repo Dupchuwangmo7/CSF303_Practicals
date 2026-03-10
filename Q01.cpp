380b62616c017320365ea98d6589f81fb57a8d79
Dupchu Wangmo <dupchuuw@Dupchus-MacBook-Air.local>
2026-03-10 21:25:01 +0600

    first commit

---
+++ Q01.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name = "Dupchu Wangmo";
+    string studentNo = "02230282";   // Store as string
+    string course = "BE Software Engineering";
+
+    cout << "=============================\n";
+    cout << "  Student Profile\n";
+    cout << "=============================\n";
+    cout << "Name         : " << name << endl;
+    cout << "Student No   : " << studentNo << endl;
+    cout << "Course       : " << course << endl;
+    cout << "=============================\n";
+
+    return 0;
+}
